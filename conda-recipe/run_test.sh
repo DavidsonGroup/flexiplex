3691dc1f0f7de20938056905cb65b51e14ed9a19
Changqing Wang <wang.ch@wehi.edu.au>
2023-10-24 16:32:17 +1100

    update conda test

--- conda-recipe/run_test.sh
+++ conda-recipe/run_test.sh
@@ -9,20 +9,14 @@ shell() {
   diff <(curl -sL https://raw.githubusercontent.com/mritchielab/FLAMES/devel/inst/extdata/bc_allow.tsv.gz | zcat) \
     <(curl -sL https://raw.githubusercontent.com/mritchielab/FLAMES/devel/inst/extdata/bc_allow.tsv.gz | zcat) > /dev/null
 )
+  msg shell env is fine
 }
 
 testflexiplex() {
   msg Testing flexiplex demultiplex output 
   diff <(flexiplex -x CTACACGACGCTCTTCCGATCT -b '????????????????' -u '????????????' -x TTTTTTTTT -e 2 -f 8 -k <(curl -sL https://raw.githubusercontent.com/mritchielab/FLAMES/devel/inst/extdata/bc_allow.tsv.gz | zcat) <(curl -sL https://raw.githubusercontent.com/mritchielab/FLAMES/devel/inst/extdata/fastq/musc_rps24.fastq.gz | zcat)) <(curl -sL https://raw.githubusercontent.com/mritchielab/FLAMES/devel/tests/testthat/demultiplexed.fq) > /dev/null
-  msg done
+  msg flexiplex demultiplex output passed
 }
-
-testflexiplexfail() {
-  msg Testing fail
-  diff <(flexiplex -x CTACACGACGCTCTTCCGATCT -b '????????????????' -u '????????????' -x TTTTTTTTT -e 2 -f 1 -k <(curl -sL https://raw.githubusercontent.com/mritchielab/FLAMES/devel/inst/extdata/bc_allow.tsv.gz | zcat) <(curl -sL https://raw.githubusercontent.com/mritchielab/FLAMES/devel/inst/extdata/fastq/musc_rps24.fastq.gz | zcat) 2>/dev/null) <(curl -sL https://raw.githubusercontent.com/mritchielab/FLAMES/devel/tests/testthat/demultiplexed.fq) > /dev/null
-  msg unexpected line reached
-}
-
 
 msg Running in $PWD
 if [[ -n "${1:-}" ]]; then
@@ -32,5 +26,6 @@ else
   # Run all code-quality tools.
   shell
   testflexiplex
-  testflexiplexfail
 fi
+echo "---"
+echo All tests passed!
